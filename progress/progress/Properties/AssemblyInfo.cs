14b12a19fd8f6935c7d567509e3cfb088eaf9ae0
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2021-06-27 23:43:05 +0000

    Add files via upload

---
+++ progress/progress/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("File System Watch (FSWatch) Beta2")]
+[assembly: AssemblyDescription("This Appliaction Published by Damon Mohammadbagher")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(" ")]
+[assembly: AssemblyProduct("File System Watch - Beta2")]
+[assembly: AssemblyCopyright("Copyright ©  2006-2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d805631d-8101-452f-a3d4-dcc8e980d4d3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.4.463.1031")]
+[assembly: AssemblyFileVersion("1.4.463.1031")]
